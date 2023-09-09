8c3ba3532dbccca996f4198a7669b1f5303eb20f
Ghaith <ghaith990000@gmail.com>
2023-09-09 22:49:21 +0300

    add admin routes

--- routes/api.php
+++ routes/api.php
@@ -29,11 +29,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::middleware('auth:sanctum')->group(function(){
     // Product Routes
-    Route::post('/products',[ProductController::class, 'store']);
-    Route::get('/products', [ProductController::class, 'index']);
-    Route::get('/products/{id}', [ProductController::class, 'show']);
-    Route::put('/products/{id}', [ProductController::class, 'update']);
-    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+
 
     // Category Routes
     Route::post('/categories', [CategoryController::class, 'store']);
@@ -42,4 +38,12 @@ Route::middleware('auth:sanctum')->group(function(){
     Route::put('/categories/{id}', [CategoryController::class, 'update']);
     Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
 
+});
+
+Route::middleware('auth:sanctum', 'admin')->group(function(){
+    Route::post('/products',[ProductController::class, 'store']);
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::get('/products/{id}', [ProductController::class, 'show']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
 });
